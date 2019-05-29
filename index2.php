7fed90c771287399ea3bdc698a4525cc2d7352ad
Anoop <48412204+Anoop684@users.noreply.github.com>
2019-05-29 06:05:52 -0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
+
 	   <div class="video-wrap">
      <video src="" playesinline autoplay id="video"></video>
    </div>
@@ -12,5 +6,3 @@
      <button id="snap">Capture</button>
    </div>
    <canvas id="canvas"></canvas>
-</body>
-</html>
