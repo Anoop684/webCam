ce5e13dcd6da3f3bbbfaff453bb315114c311e46
Anoop <48412204+Anoop684@users.noreply.github.com>
2019-05-29 06:43:59 -0300

    Delete index2.php

--- index2.php
+++
@@ -1,8 +0,0 @@
-
-	   <div class="video-wrap">
-     <video src="" playesinline autoplay id="video"></video>
-   </div>
-   <div class="controller">
-     <button id="snap">Capture</button>
-   </div>
-   <canvas id="canvas"></canvas>
